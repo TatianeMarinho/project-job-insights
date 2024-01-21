08d34093bf500d2aa732d9d6aacb84616066ea11
TatianeMarinho <devtatianemarinho@gmail.com>
2024-01-21 18:47:41 -0400

    método get_unique_job_types

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -13,7 +13,18 @@ class ProcessJobs:
             return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
-        pass
+        #   cria um conjunto vazio(que armazem valores unicos)
+        unique_job_types = set()
+        #   iterar sobre trabalho(dicionario) na lista de trabalhos
+        for job in self.jobs_list:
+            #   obtem o tipo de trabalho(get retorna None se nao existir)
+            job_type = job.get('job_type')
+            #   se existi um job_type mesmo tirando espaço em branco iniciofim
+            if job_type and job_type.strip():
+                #    adiciona o job_type ao conjunto
+                unique_job_types.add(job['job_type'])
+        #   converte o conjunto em uma lista e a retorna
+        return list(unique_job_types)
 
     def filter_by_multiple_criteria(self) -> List[dict]:
         pass
