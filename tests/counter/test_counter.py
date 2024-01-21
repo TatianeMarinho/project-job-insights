aca34e89db41431410af06d9a3addfc76343e3a4
TatianeMarinho <devtatianemarinho@gmail.com>
2024-01-21 18:05:01 -0400

    Count Ocurrences (Testes)

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,11 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+
+
+PATH = "data/jobs.csv"
+#   PATHINVALID = "data/jobs_invalid.csv"
 
 
 def test_counter():
-    raise
+    assert count_ocurrences(PATH, "Python") == 1639
+    assert count_ocurrences(PATH, "Java") == 676
+#   assert count_ocurrences(PATHINVALID, "Python") == 0
