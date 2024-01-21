a19977a94d6790874d2cf365b46af0a87bd7057e
TatianeMarinho <devtatianemarinho@gmail.com>
2024-01-21 19:28:38 -0400

    método get_unique_industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -7,4 +7,9 @@ class ProcessIndustries(ProcessJobs):
         super().__init__()
 
     def get_unique_industries(self) -> List[str]:
-        pass
+        unique_industries = set()
+        for job in self.jobs_list:
+            industry = job.get('industry')
+            if industry and industry.strip():
+                unique_industries.add(job['industry'])
+        return list(unique_industries)
